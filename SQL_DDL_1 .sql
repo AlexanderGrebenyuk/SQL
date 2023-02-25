cfe1ff7b635cced98da8b92d94ead6dbe10c999c
AlexanderGrebenyuk <113853997+AlexanderGrebenyuk@users.noreply.github.com>
2023-02-25 11:42:04 +0300

    Update SQL_DDL_1 .sql

--- SQL_DDL_1 .sql
+++ SQL_DDL_1 .sql
@@ -1,16 +1,16 @@
-//1. Таблица employees
+//1. РўР°Р±Р»РёС†Р° employees
 
-//Создать таблицу employees
+//РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 //id. serial,  primary key,
 //employee_name. Varchar(50), not null
 
-сreate table employees(
+Г±reate table employees(
 id serial primary key,
 employee_name Varchar(50) not null
 );
 
 
-//Наполнить таблицу employee 70 строками.
+//РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё
 
 insert into employees (employee_name)
 values ('Natalie'),
@@ -84,9 +84,9 @@ values ('Natalie'),
        ('Gary'),	 
        ('Eric');
       
-//2. Таблица salary
+//2. РўР°Р±Р»РёС†Р° salary
 
-//Создать таблицу salary
+//РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
 //id. Serial  primary key,
 //monthly_salary. Int, not null
       
@@ -94,7 +94,7 @@ values ('Natalie'),
 	id serial primary key,
 	monthly_salary int not null
 );
-//Наполнить таблицу salary 15 строками:
+//РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 16 СЃС‚СЂРѕРєР°РјРё:
 //1000
 //1100
 //1200
@@ -132,9 +132,9 @@ values (1000),
       
       select * from employees;
 
-//3. Таблица employee_salary
+//3. РўР°Р±Р»РёС†Р° employee_salary
 
-//Создать таблицу employee_salary
+//РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 //id. Serial  primary key,
 //employee_id. Int, not null, unique
 //salary_id. Int, not null
@@ -144,8 +144,8 @@ values (1000),
 	employee_id int not null unique,
 	salary_id int not null
 );
-//Наполнить таблицу employee_salary 40 строками:
-//в 10 строк из 40 вставить несуществующие employee_id
+//РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
+//РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
 
 insert into employee_salary (employee_id, salary_id)
 values (1, 1),
@@ -192,16 +192,16 @@ values (1, 1),
 
 select * from employee_salary;
 
-//4. Таблица roles
+//4. РўР°Р±Р»РёС†Р° roles
 
-//Создать таблицу roles
+//РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 //id. Serial  primary key,
 //role_name. int, not null, unique
 create table roles (
 	id serial primary key,
 	role_name int not null unique
 );
-//Поменять тип столба role_name с int на varchar(30)
+//РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
 ALTER TABLE roles 
 ALTER COLUMN role_name
 TYPE varchar(30);
@@ -210,7 +210,7 @@ select column_name, data_type
 FROM  information_schema.columns
 WHERE table_name = 'roles';
 
-//Наполнить таблицу roles 20 строками:
+//РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё:
 
 insert into roles (role_name)
 values ('Junior Python developer'),
@@ -237,11 +237,11 @@ values ('Junior Python developer'),
 	  select * from roles_employee;
 	 
 	 
-	-- 5. Таблица roles_employee
-//Создать таблицу roles_employee
+	-- 5. РўР°Р±Р»РёС†Р° roles_employee
+//РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 //id. Serial  primary key,
-//employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
-//role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+//employee_id. Int, not null, unique (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ employees, РїРѕР»Рµ id)
+//role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
 
 	 create table roles_employee(
 	id serial  primary key,
@@ -253,7 +253,7 @@ values ('Junior Python developer'),
 		references roles(id)
 );
 
-//Наполнить таблицу roles_employee 40 строками:
+//РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё:
 
 insert into roles_employee (employee_id, role_id)
 values (1,1),
